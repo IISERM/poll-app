7b1a6acdacc6adce537b8dadaf844a514edad8c7
James471 <56861963+James471@users.noreply.github.com>
2020-08-04 17:36:51 +0530

    Update polls.js

--- js/polls.js
+++ js/polls.js
@@ -11,9 +11,8 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 var db = firebase.firestore();
-var userGlobal;
+var pollGlobal;
 firebase.auth().onAuthStateChanged(function (user) {
-    userGlobal = user;
     if (user) {
         if (!user.emailVerified) {
             displayMessage("You need to verify your email before you can cast your votes.");
@@ -24,7 +23,7 @@ firebase.auth().onAuthStateChanged(function (user) {
     }
 });
 function loadActivePolls() {
-    //The function logic would be different once the lists are there. There will be one document storing the names of active polls for each list. This function will query that document.
+    //The function logic would be different once the lists are there.
     //Write now, there is just one db having the name of all polls.
     var activePolls;
     var select = document.getElementById("pollselect");
@@ -61,8 +60,8 @@ function loadPollQuestions() {
                         displayMessage("An unexpected error has occurred. Report to the developers.");
                         console.log("The document was not found.");
                     } else {
-                        poll = doc.data();
-                        document.getElementById('current_poll').innerHTML = poll.getAsHTML();
+                        pollGlobal = doc.data();
+                        document.getElementById('current_poll').innerHTML = pollGlobal.getAsHTML();
                     }
                 })
                 .catch(function (error) {
@@ -73,6 +72,8 @@ function loadPollQuestions() {
         } else {
             document.getElementById('current_poll').innerHTML = "Yay! You've already voted!"
         }
+    } else {
+	pollGlobal = null;
     }
 }
 
@@ -87,18 +88,75 @@ function displayMessage(msg) {
 }
 
 function hasAlreadyVoted() {
-    return false;
+    var alreadyVotedList = null;
+    db.collection("Polls").doc("Redundant").collection(pollGlobal.topic).doc("PeopleWhoHaveAlreadyVoted")
+        .get()
+        .then(function(doc){
+            alreadyVotedList = doc.AlreadyVoted;
+            if(alreadyVotedList.indexOf(firebase.auth().currentUser.uid)!=-1) {
+                return false;
+	        } else {
+                return true;
+	        }
+		})
+        .catch(function(error) {
+            displayMessage("There was an error. Please retry.");
+            console.log(error.code, error.message);
+		});
+}
+
+function submitPollResponse() {
+    var numOfQuestions = pollGlobal.questions.length;
+    var i;
+    var dbRef = db.collection("Polls").doc("Redundant").collection(pollGlobal.topic).doc("PollResults");
+    if(pollGlobal.isAnonymous){
+        var batch = db.batch();
+        var responseMap = {};
+        for(i=0; i<numOfQuestions; i++) {
+            var qType = pollGlobal.questions[i].type;
+            if (qType == 0) {
+                var j;
+                for(j=0; j<pollGlobal.questions[i].options.length; j++) {
+                    if(document.getElementById("question_"+i+"_"+j).checked) {
+                        responseMap[pollGlobal.questions[i].questionStr+"."+pollGlobal.questions[i].options[j]] = firebase.firestore.FieldValue.increment(1);
+                        break;
+				    }
+			    }
+		    } else if (qType == 1) {
+                var j;
+                for(j=0; j<pollGlobal.questions[i].options.length; j++) {
+                    if(document.getElementById("question_"+i+"_"+j).checked) {
+                        responseMap[pollGlobal.questions[i].questionStr+"."+pollGlobal.questions[i].options[j]] = firebase.firestore.FieldValue.increment(1);
+				    }
+			    }
+		    } else if (qType == 2) {
+                var resp = document.getElementById("question_"+i).value;
+                responseMap[pollGlobal.questions[i].questionStr+".Responses"] = firebase.firestore.FieldValue.arrayUnion(resp);
+		    } else {
+                console.log("Something is seriously wrong.");
+		    }
+	    }
+        batch.update(dbRef, responseMap);
+        batch.update(db.collection("Polls").doc("Redundant").collection(pollGlobal.topic).doc("PeopleWhoHaveAlreadyVoted"), {"AlreadyVoted": firebase.firestore.FieldValue.arrayUnion(firebase.auth().currentUser.uid)});
+        batch.commit()
+            .then(function() {
+                displayMessage("Your response has been recorded.");
+                window.location.reload();
+		    })
+            .catch(function(error) {
+                displayMessage("Your response couldn't be recorded");
+                console.log(error.code, error.message);
+		    });
+    }
 }
 
 function signOut() {
     firebase.auth().signOut()
-        .then(function () {
-            location.href = "index.html"
-        }, function (error) {
+        .catch(function(error) {
             displayMessage("Couldn't sign you out.");
             console.log(error.code);
             console.log(error.message);
-        });
+		});
 }
 
 /*
